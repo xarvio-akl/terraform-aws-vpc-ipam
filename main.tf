f06f437c19989c279787dd8b720c64010780ee46
Andre Klocke <rakon@helawann.de>
2022-12-09 16:47:46 +0100

    changes to for_each

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc_ipam" "this" {
   description = var.description
   dynamic "operating_regions" {
-    for_each = var.regions
+    for_each = toset(var.regions)
     content {
-      region_name = regions.value
+      region_name = each.key
     }
   }
 }
