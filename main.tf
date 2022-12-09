e4ed1edd9b855796f5c8190b8d89a71a05c50984
Andre Klocke <rakon@helawann.de>
2022-12-09 16:55:01 +0100

    new test

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc_ipam" "this" {
   description = var.description
-  dynamic "operating_regions" {
+  dynamic operating_regions {
     for_each = toset(var.regions)
     content {
-      region_name = each.key
+      region_name = operating_regions.value
     }
   }
 }
